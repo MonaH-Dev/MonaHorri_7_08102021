5d2c493db505514b02b4a3b3a208353d1cd8cff6
MonaH-Dev <Chtipou@Air-de-Chtipou.home>
2021-10-15 13:56:31 +0200

    après mentorat du 15/10

--- projet/index.js
+++ projet/index.js
@@ -1,12 +1,35 @@
 console.log(recipes[0]);
 
-const test = "chocolat";
+const $seachField = document.querySelector(".searchbox input");
+// memo : faire commencer par "$" les elt HTML
+$seachField.addEventListener('input', getSearchResult)
 
-const filterArray = [];
-for (let i = 0; i < recipes.length; i ++) {
-  // console.log(recipes[i]);
- if (recipes[i].name.toLowerCase().match(test)) {
-  filterArray.push(recipes[i])
- }
+function getSearchResult (e) {
+  const searchText = e.target.value;
+  if (searchText.length > 3) {
+    const filterArray = [];
+    for (let i = 0; i < recipes.length; i ++) {
+      // console.log(recipes[i]);
+      if (recipeTextMatchWithSearchText(recipes[i].name, searchText) ||
+      recipeTextMatchWithSearchText(recipes[i].description, searchText) ||
+      recipeIngredientsMatchWithSearchText(recipes[i].ingredients, searchText) ) {
+        filterArray.push(recipes[i])
+      }
+    }
+    console.log(filterArray)
+  }
 }
-console.log(filterArray)
+
+
+function recipeTextMatchWithSearchText (recipeText, searchText) {
+  return recipeText.toLowerCase().match(searchText)
+}
+
+function recipeIngredientsMatchWithSearchText (recipeIngredients, searchText) {
+  for (let i = 0; i < recipeIngredients.length; i ++) {
+    if (recipeIngredients[i].ingredient.toLowerCase().match(searchText)) {
+      return true;
+    }
+  }  
+  return false;
+}
