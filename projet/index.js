e0aff3f0cdaae4e80017bd4063627a547622d606
MonaH-Dev <Chtipou@Air-de-Chtipou.home>
2021-10-18 14:05:13 +0200

    après mentorat du 18/10

--- projet/index.js
+++ projet/index.js
@@ -1,35 +1,78 @@
+const recipeCtr = document.querySelector(".TheRecipes");
+
+function addElt(recipe) {
+  let innerHTML = `<article class="recipe">
+  <div class="recipePic"></div>
+  <div class="recipePrev">
+    <div class="recipe-prez">
+      <h2>${recipe.name}</h2>
+      <div class="recipe-prez__timing">
+        <img
+          class="recipe-prez__timing--icon"
+          src="../projet/img/time.svg"
+          alt="timer icon"
+        />
+        <p class="recipe-prez__timing--duration">${recipe.time} min</p>
+      </div>
+    </div>
+    <div class="recipe-details">
+      <div class="recipe-details__ingredient">
+        <ul>`;
+  for (let i = 0; i < recipe.ingredients.length; i++) {
+    innerHTML += `<li>
+          <strong class="OneIgdt">${recipe.ingredients[i].ingredient}</strong>: 
+          ${recipe.ingredients[i].quantity || ""}${
+      recipe.ingredients[i].unit || ""
+    }
+        </li>`;
+  }
+  innerHTML += `</ul>
+      </div>
+      <div class="recipe-details__preparation">
+      ${recipe.description}
+      </div>
+    </div>
+  </div>
+  </article>
+  `;
+  recipeCtr.innerHTML += innerHTML;
+}
+
+//#region CARACTÈRES TAPÉS DANS LA BARRE DE RECHERCHE
 console.log(recipes[0]);
 
 const $seachField = document.querySelector(".searchbox input");
 // memo : faire commencer par "$" les elt HTML
-$seachField.addEventListener('input', getSearchResult)
+$seachField.addEventListener("input", getSearchResult);
 
-function getSearchResult (e) {
+function getSearchResult(e) {
   const searchText = e.target.value;
   if (searchText.length > 3) {
     const filterArray = [];
-    for (let i = 0; i < recipes.length; i ++) {
+    for (let i = 0; i < recipes.length; i++) {
       // console.log(recipes[i]);
-      if (recipeTextMatchWithSearchText(recipes[i].name, searchText) ||
-      recipeTextMatchWithSearchText(recipes[i].description, searchText) ||
-      recipeIngredientsMatchWithSearchText(recipes[i].ingredients, searchText) ) {
-        filterArray.push(recipes[i])
+      if (
+        recipeTextMatchWithSearchText(recipes[i].name, searchText) ||
+        recipeTextMatchWithSearchText(recipes[i].description, searchText) ||
+        recipeIngredientsMatchWithSearchText(recipes[i].ingredients, searchText)
+      ) {
+        filterArray.push(recipes[i]);
+        addElt(recipes[i]);
       }
     }
-    console.log(filterArray)
   }
 }
 
-
-function recipeTextMatchWithSearchText (recipeText, searchText) {
-  return recipeText.toLowerCase().match(searchText)
+function recipeTextMatchWithSearchText(recipeText, searchText) {
+  return recipeText.toLowerCase().match(searchText);
 }
 
-function recipeIngredientsMatchWithSearchText (recipeIngredients, searchText) {
-  for (let i = 0; i < recipeIngredients.length; i ++) {
+function recipeIngredientsMatchWithSearchText(recipeIngredients, searchText) {
+  for (let i = 0; i < recipeIngredients.length; i++) {
     if (recipeIngredients[i].ingredient.toLowerCase().match(searchText)) {
       return true;
     }
-  }  
+  }
   return false;
 }
+//#endregion
