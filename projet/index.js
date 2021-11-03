6ffdfed5b0c77ed2db488bfb276c5cbe4217bc39
MonaH-Dev <Chtipou@MacBook-Air-de-Chtipou.local>
2021-11-03 19:25:19 +0100

    récupérer listes nettoyées OK

--- projet/index.js
+++ projet/index.js
@@ -98,37 +98,82 @@ console.log("Start flag");
 //#endregion
 
 //#region VISUALISATION DES LISTES / FILTRES
-// ---------------------------- 1er essai
-// function showLists() {
-//   document.getElementById("ingredientsList").classList.add("show");
-// }
-let $rechercherDsList = document.querySelector(".Block");
-// if ($rechercherDsList) {
-// $rechercherDsList.addEventListener("click", showLists());
-// }
-// ---------------------------- 1er essai </
 
 // ---------------------------- 2e essai
-$rechercherDsList.addEventListener("mouseover", show_hide);
-function show_hide() {
-  var showElt = document.getElementById("ingredientsList");
-  if (showElt.style.display === "none") {
-    showElt.style.display = "flex";
-  } else {
-    showElt.style.display = "none";
-  }
-}
+// $rechercherDsList.addEventListener("mouseover", show_hide);
+// function show_hide() {
+//   var showElt = document.getElementById("ingredientsList");
+//   if (showElt.style.display === "none") {
+//     showElt.style.display = "flex";
+//   } else {
+//     showElt.style.display = "none";
+//   }
+// }
 // ---------------------------- 2e essai </
+const listCtr = document.querySelector("#ingredientsList");
+const allChevrons = document.querySelectorAll(".chevron");
+allChevrons.forEach((ch) =>
+  ch.addEventListener("click", function (e) {
+    let parentBlockId = e.target.closest(".Block").id;
+    console.log(parentBlockId);
+    getAdvancedSearchFieldList(parentBlockId);
+  })
+);
+
+// Peuple le champs de recherche avancée fourni en parametres
+function getAdvancedSearchFieldList(blockId) {
+  // 1 - Recuperer une liste de données en fonciton du type d'input ( ingredients, appareils, ustensils )
+  let rawData = [];
+  switch (blockId) {
+    case "ingredientsBlock":
+      recipes.forEach((r) => {
+        rawData.push(...r.ingredients.map((i) => i.ingredient));
+      });
+      break;
+    case "appareilBlock":
+      recipes.forEach((r) => {
+        rawData.push(r.appliance); // "..." permet de recuperer le contenu du tableau plutot que le tableau lui meme
+      });
+      break;
+    case "ustensilesBlock":
+      recipes.forEach((r) => {
+        rawData.push(...r.ustensils);
+      });
+      break;
+  }
+  // 2 - Nettoyer la liste ( pas de doublons )
+  let data = new Set(rawData);
+  console.log(data);
+  // 3 - Filtrer les données selon la recherche courante
+  // 4 - Peupler le champs de recherche avancée correspondant
+  addIngList();
+}
+
+function addIngList(ingredient) {
+  let innerHTML;
+  // for (let i = 0; i < recipes.ingredients.length; i++) {
+  //   innerHTML += `<a>${recipes.ingredients[i].ingredient}</a>`;
+  // }
+  // console.log(innerHTML);
+  // 1 : ajouter une balise <a>
+  // 2 : ajouter un ingrédient dans la balise
+  // 3 : faire ça pour tous les ingrédients
+  // 4 : garder 1x un ingrédient
+
+  // recipeCtr.innerHTML += innerHTML;
+}
+addIngList();
 
 //#endregion
 
 //#region CHANGE THE PLACEHOLDER
+let $rechercherDsList = document.querySelector(".Block");
 placeholderText = document.querySelector('[placeholder="Ingrédients"]');
 function ph() {
   placeholderText.setAttribute("placeholder", "Ingrédients");
 }
 function phr() {
-  placeholderText.setAttribute("placeholder", "Recherche un ingédient");
+  placeholderText.setAttribute("placeholder", "Recherche un ingrédient");
 }
 $rechercherDsList.addEventListener("mouseover", phr);
 $rechercherDsList.addEventListener("mouseout", ph);
