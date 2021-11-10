d0ff6bd5b448dca894ff9d4101e9ff5b0b15aa33
MonaH-Dev <80840491+MonaH-Dev@users.noreply.github.com>
2021-11-10 18:22:59 +0100

    nettoyage v1

--- projet/index.js
+++ projet/index.js
@@ -1,59 +1,16 @@
-const recipeCtr = document.querySelector(".TheRecipes");
-let currentTags = []; // Tags actifs
-let mainTextInput = ""; // Texte dans la barre de recherche
-//#region CONSOLE.LOG
-
-// recipes[0].ingredients[3].unit;
-// console.log(recipes[0].ingredients[3].unit);
-
-//#endregion
-
-//#region FONCTION TO ADD HTML (in TheRecipes)
-function addElt(recipe) {
-  let innerHTML = `<article class="recipe">
-  <div class="recipePic"></div>
-  <div class="recipePrev">
-    <div class="recipe-prez">
-      <h2>${recipe.name}</h2>
-      <div class="recipe-prez__timing">
-        <img
-          class="recipe-prez__timing--icon"
-          src="../projet/img/time.svg"
-          alt="timer icon"
-        />
-        <p class="recipe-prez__timing--duration">${recipe.time} min</p>
-      </div>
-    </div>
-    <div class="recipe-details">
-      <div class="recipe-details__ingredient">
-        <ul>`;
-  for (let i = 0; i < recipe.ingredients.length; i++) {
-    innerHTML += `<li>
-          <strong class="OneIgdt">${recipe.ingredients[i].ingredient}</strong>:
-          ${recipe.ingredients[i].quantity || ""}${
-      recipe.ingredients[i].unit || ""
-    }
-        </li>`;
-  }
-  innerHTML += `</ul>
-      </div>
-      <div class="recipe-details__preparation">
-      ${recipe.description}
-      </div>
-    </div>
-  </div>
-  </article>
-  `;
-  recipeCtr.innerHTML += innerHTML;
-}
-//#endregion
-
-//#region CARACTÈRES TAPÉS DANS LA BARRE DE RECHERCHE
-
+//#region CONSTANTES ET VARIABLES
 // memo : faire commencer par "$" les elt HTML
-const $seachField = document.querySelector(".searchbox input");
-let filteredRecipes;
-const blockIds = ["ingredientsBlock", "appareilBlock", "ustensilesBlock"];
+const $recipeCtr = document.querySelector(".TheRecipes"); //Ctr des recettes
+let currentTags = []; //Tags actifs
+let mainTextInput = ""; //Texte dans la barre de recherche
+const $seachField = document.querySelector(".searchbox input"); //Barre de recherche
+let filteredRecipes; //recettes filtrées
+const blockIds = ["ingredientsBlock", "appareilBlock", "ustensilesBlock"]; //tableau avec l'ID de chaque liste
+const $allChevrons = document.querySelectorAll(".chevron"); //tous les chevrons
+
+//#endregion
+
+//#region EVENT - BARRE DE RECHERCHE
 // On écoute la valeur entrée dans la barre de recherche :
 $seachField.addEventListener("input", function (e) {
   //console.log("New input");
@@ -66,10 +23,23 @@ $seachField.addEventListener("input", function (e) {
   });
   //console.log("Event flag");
 });
+//#endregion
 
+//#region EVENT - CHEVRONS ET AUTRES ( !!!! A FAIRE !!!!! )
+$allChevrons.forEach((ch) =>
+  ch.addEventListener("click", function (e) {
+    console.log("Chevron clicked");
+    let parentBlockId = e.target.closest(".Block").id;
+    console.log(parentBlockId);
+    updateAdvancedSearchField(parentBlockId);
+  })
+);
+//#endregion
+
+//#region FONCTION - Mets a jour les recettes selon l'input
 // Mets a jour les recettes selon l'input
 function updateSearchResult(inputTxt = "", tagMode = false) {
-  console.log(tagMode);
+  // console.log(tagMode);
   if (
     tagMode ||
     currentTags.length > 0 ||
@@ -77,7 +47,7 @@ function updateSearchResult(inputTxt = "", tagMode = false) {
     inputTxt == ""
   ) {
     console.log("Update UI");
-    recipeCtr.innerHTML = "";
+    $recipeCtr.innerHTML = "";
     filteredRecipes = [];
     for (let i = 0; i < recipes.length; i++) {
       // console.log(recipes[i]);
@@ -94,6 +64,13 @@ function updateSearchResult(inputTxt = "", tagMode = false) {
     // console.log("Filtered Recipes ", filteredRecipes);
   }
 }
+// Premier lancement de la fonction :
+// c'est la fonction par défaut,
+updateSearchResult();
+console.log("Start flag");
+//#endregion
+
+//#region FONCTIONS - Chercher correspondance txt tapé VS autres
 // Fonctions de vérifs (nom et description), avec 2 paramètres,
 // le 2nd est mis en minuscule, puis comparé au 1er
 // Retourne true si les inputs (searchTxt && currentTags) correspondent a la recette courante
@@ -134,27 +111,49 @@ function recipeIngredientsMatchWithSearchText(recipeIngredients, searchText) {
   console.log("Matching = ", result);
   return result;
 }
-
-// Premier lancement de la fonction :
-// c'est la fonction par défaut,
-updateSearchResult();
-console.log("Start flag");
-
 //#endregion
 
-//#region VISUALISATION DES LISTES / FILTRES
+//#region FONCTION - To add HTML (in TheRecipes)
+function addElt(recipe) {
+  let innerHTML = `<article class="recipe">
+  <div class="recipePic"></div>
+  <div class="recipePrev">
+    <div class="recipe-prez">
+      <h2>${recipe.name}</h2>
+      <div class="recipe-prez__timing">
+        <img
+          class="recipe-prez__timing--icon"
+          src="../projet/img/time.svg"
+          alt="timer icon"
+        />
+        <p class="recipe-prez__timing--duration">${recipe.time} min</p>
+      </div>
+    </div>
+    <div class="recipe-details">
+      <div class="recipe-details__ingredient">
+        <ul>`;
+  for (let i = 0; i < recipe.ingredients.length; i++) {
+    innerHTML += `<li>
+          <strong class="OneIgdt">${recipe.ingredients[i].ingredient}</strong>:
+          ${recipe.ingredients[i].quantity || ""}${
+      recipe.ingredients[i].unit || ""
+    }
+        </li>`;
+  }
+  innerHTML += `</ul>
+      </div>
+      <div class="recipe-details__preparation">
+      ${recipe.description}
+      </div>
+    </div>
+  </div>
+  </article>
+  `;
+  $recipeCtr.innerHTML += innerHTML;
+}
+//#endregion
 
-const $listCtr = document.querySelector("#ingredientsList");
-const $allChevrons = document.querySelectorAll(".chevron");
-$allChevrons.forEach((ch) =>
-  ch.addEventListener("click", function (e) {
-    console.log("Chevron clicked");
-    let parentBlockId = e.target.closest(".Block").id;
-    console.log(parentBlockId);
-    updateAdvancedSearchField(parentBlockId);
-  })
-);
-
+//#region FONCTION - Mets a jour les ASF selon la liste de recettes
 // Mets a jour les asf selon la liste de recettes ("filteredRecipes")
 function updateAdvancedSearchField(blockId) {
   // 1 - Récuperer une liste de données en fonction du type d'input ( ingredients, appareils, ustensils )
@@ -184,22 +183,14 @@ function updateAdvancedSearchField(blockId) {
   // 3 - Peupler le champs de recherche avancée correspondant
   populateAsf(blockId, data);
 }
+//#endregion
+
+//#region !!!!! A REORDONNER !!!!!! VISUALISATION DES LISTES / FILTRES
 
 // Peupler un block ASF (selon blockID)
 // blockID : string servant à identifier le block à màj
 // asfData : liste de données selon le block à màj = set(rawData)
 function populateAsf(blockId, asfData) {
-  //--------- si besoin de conversion : -------------
-  // const dataList = [...asfData];
-  // console.log("DataList ", dataList);
-  // console.log("DataList with ... --> ", [...asfData]);
-  //const dL = [asfData[0],asfData[1],asfData[2]] soit un tableau de 30 elt
-  //Versus :
-  // const dl2 = [asfData];
-  // console.log("DataList without ... --> ", [asfData]);
-  //const dl2 = soit un tableau avec 1 seul elt (1tableau), contenant lui-même 30 elt
-  //--------- fin de conversion -------------------
-
   // 1 - Recuperer l'element HTML (div) qui va contenir les données (ingredients, appareils, ustensils)
   let $asfRoot;
   if (blockIds.includes(blockId)) {
@@ -214,7 +205,6 @@ function populateAsf(blockId, asfData) {
     // console.log("DL = ", data);
     htmlToInject += `<a href="#">${data}</a><br>`;
   });
-
   $asfRoot.innerHTML += htmlToInject;
 
   // Ajouter un event sur chaque lien contenu dans un asfList
@@ -272,7 +262,7 @@ function populateAsf(blockId, asfData) {
 
 //#endregion
 
-//#region CHANGE THE PLACEHOLDER
+//#region CHANGE THE PLACEHOLDER ( !!!! code à revoir !!! )
 let $rechercherDsList = document.querySelector(".Block");
 placeholderText = document.querySelector('[placeholder="Ingrédients"]');
 function ph() {
@@ -285,6 +275,102 @@ $rechercherDsList.addEventListener("mouseover", phr);
 $rechercherDsList.addEventListener("mouseout", ph);
 //#endregion
 
-//#region
+//#region XXX
+
+//#endregion
+
+//#region CONSOLE.LOG
+// recipes[0].ingredients[3].unit;
+// console.log(recipes[0].ingredients[3].unit);
+//#endregion
+
+//#region fonction populateAsf - étape par étape
+// // Peupler un block ASF (selon blockID)
+// // blockID : string servant à identifier le block à màj
+// // asfData : liste de données selon le block à màj = set(rawData)
+// function populateAsf(blockId, asfData) {
+//   // 1 - Recuperer l'element HTML (div) qui va contenir les données (ingredients, appareils, ustensils)
+//   let $asfRoot;
+//   if (blockIds.includes(blockId)) {
+//     $asfRoot = document.querySelector(`.${blockId} .asfList`);
+//   }
+//   // 2 - Vider l'element div (.asfList)
+//   $asfRoot.innerHTML = "";
+
+//   // 3 - Ajouter des elements a notre div (.asfList)
+//   let htmlToInject = "";
+//   asfData.forEach((data) => {
+//     // console.log("DL = ", data);
+//     htmlToInject += `<a href="#">${data}</a><br>`;
+//   });
+
+//   $asfRoot.innerHTML += htmlToInject;
+
+//   // Ajouter un event sur chaque lien contenu dans un asfList
+
+//   // 1 - Recuperer tous les liens de l'ASF list courant
+//   let $asfLinks = document.querySelectorAll(`.${blockId} .asfList a`);
+//   const $tagsCtr = document.querySelector(".tags");
+//   // 2 - Ajouter un evenement sur chaque lien
+//   $asfLinks.forEach((link) =>
+//     link.addEventListener("click", function (e) {
+//       //console.log("Click on asf element", link);
+//       updateSearchResult(mainTextInput, true);
+
+//       // 1 - Recuperer le text du lien
+//       let tagName = link.textContent;
+
+//       // 1b - Verifier si le tag est deja actif, si oui arreter l'execution ici (return)
+//       // find => Cherche un element dans une liste donnée qui correspond aux criteres
+//       if (currentTags.find((t) => t.innerText === tagName)) return;
+
+//       // 2 - Créer un nouveau tag
+//       let $tag = document.createElement("div");
+//       $tag.className = "tag";
+//       $tag.innerHTML = `
+//       <div class="tag-txt">${tagName}</div>
+//       `;
+
+//       // 3 - Creer le button close du tag
+//       let $tagCloseBtn = document.createElement("img");
+//       $tagCloseBtn.className = "tag-img";
+//       $tagCloseBtn.src = "../projet/img/cross.svg";
+//       $tagCloseBtn.alt = "closetag";
+
+//       // 4 - Ajouter le bt close au tag precedemment crée
+//       $tag.appendChild($tagCloseBtn);
+
+//       // 5 - Ajouter un event pour le tag créé (Close btn)
+//       $tagCloseBtn.addEventListener("click", function (e) {
+//         $tag.remove();
+//         // filter : supprime dans une liste les elements ne correspondants pas aux criteres
+//         currentTags = currentTags.filter((ct) => ct != $tag);
+//         console.log("Current tags = ", currentTags);
+//       });
+
+//       // 6 - Ajouter le tag a son conteneur (div)
+//       $tagsCtr.appendChild($tag);
+//       // 7 - Ajouter la reference du tag a la liste globale ()
+//       currentTags.push($tag);
+//       console.log("Current tags = ", currentTags);
+//     })
+//   );
+
+//   // Ajouter un event sur chaque bouton close de chaque tag
+// }
+//#endregion
+
+//#region Commentaires brouillon
+
+//--------- si besoin de conversion : -------------
+// const dataList = [...asfData];
+// console.log("DataList ", dataList);
+// console.log("DataList with ... --> ", [...asfData]);
+//const dL = [asfData[0],asfData[1],asfData[2]] soit un tableau de 30 elt
+//Versus :
+// const dl2 = [asfData];
+// console.log("DataList without ... --> ", [asfData]);
+//const dl2 = soit un tableau avec 1 seul elt (1tableau), contenant lui-même 30 elt
+//--------- fin de conversion -------------------//
 
 //#endregion
